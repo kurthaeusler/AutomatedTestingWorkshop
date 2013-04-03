c2672c06d87424f773e8453f742ec748f7366b48
Kurt Haeusler <kurt.haeusler@etecture.de>
2013-04-03 19:06:29 +0200

    Adding a new unit test

---
+++ src/test/java/com/kurt/atw/unit/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.kurt.atw.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.kurt.atw.Calculator;
+
+public class CalculatorTest {
+
+	@Test
+	public void testDisplay() {
+		Calculator calculator = new Calculator();
+		
+		assertEquals("0.", calculator.displayShouldBe());
+	}
+
+}
